f6e1f951d964a6617cfdffe53e5ce0b6fb48f9a2
Abhay Joshi <sychonet@gmail.com>
2020-12-22 02:23:39 +0530

    Added support for setting up Magento 2 using nginx

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-/var/www/html/magento2/bin/magento setup:install --base-url=$BASE_URL --db-host=$MAGENTO2_DB_HOST 
-  --db-name=socialshop \
-  --db-user=magento2 \
-  --db-password=magento2 \
-  --admin-firstname=fname \
-  --admin-lastname=lname \
-  --admin-email=admin@socialshop.com \
-  --admin-user=admin \
-  --admin-password=admin123 \
-  --language=en_US \
-  --currency=USD \
-  --timezone=Asia/Kolkata \
+/temp/wait-for-it.sh -t 120 shopkey_database:3306 -- echo "MySQL is up"
+/temp/wait-for-it.sh -t 120 shopkey_search:9200 -- echo "Elasticsearch is up"
+/var/www/html/magento2/bin/magento setup:install \
+  --base-url=$MAGENTO2_BASE_URL \
+  --db-host=$MAGENTO2_DB_HOST \
+  --db-name=$MAGENTO2_DB_NAME \
+  --db-user=$MAGENTO2_DB_USER \
+  --db-password=$MAGENTO2_DB_PASSWORD \
+  --admin-firstname=$MAGENTO2_ADMIN_FIRSTNAME \
+  --admin-lastname=$MAGENTO2_ADMIN_LASTNAME \
+  --admin-email=$MAGENTO2_ADMIN_EMAIL \
+  --admin-user=$MAGENTO2_ADMIN_USER \
+  --admin-password=$MAGENTO2_ADMIN_PASSWORD \
+  --language=$MAGENTO2_LANGUAGE \
+  --currency=$MAGENTO2_CURRENCY \
+  --timezone=$MAGENTO2_TIMEZONE \
   --use-rewrites=1 \
-  --elasticsearch-host=socialshop_search
-
+  --elasticsearch-host=$MAGENTO2_ELASTICSEARCH_HOST
 /var/www/html/magento2/bin/magento mo:di Magento_TwoFactorAuth
+/var/www/html/magento2/bin/magento deploy:mode:set developer
+nginx -t
+service nginx start
+php-fpm
